76b183e2bf56e73a07f48f1eefbc878398df41f3
prc <prc>
2003-01-20 09:51:53 +0000

    Templates for ObTools projects

---
+++ templates/t.h
@@ -0,0 +1,42 @@
+//==========================================================================
+// ObTools::<project>: <file>.h
+//
+// <purpose>
+//
+// Copyright (c) 2003 Object Toolsmiths Limited.  All rights reserved
+//==========================================================================
+
+#ifndef __OBTOOLS_<FILE>_H
+#define __OBTOOLS_<FILE>_H
+
+#include <string.h>
+#include <list.h>
+using namespace std;
+
+namespace ObTools { namespace <project>
+{
+
+//==========================================================================
+//
+
+
+class X
+{
+private:
+
+public:
+  //------------------------------------------------------------------------
+  //
+
+};
+
+
+
+
+
+//==========================================================================
+}} //namespaces
+#endif // !__OBTOOLS_FILE_H
+
+
+
