f0ad50aaf668a17ccfa26357d2de09b1d2fa9634
prc <prc>
2003-07-09 21:37:42 +0000

    Use namespace to make things tidier

--- libs/log/test-log.cc
+++ libs/log/test-log.cc
@@ -8,32 +8,33 @@
 
 #include "ot-log.h"
 using namespace std;
+using namespace ObTools::Log;
 
 //--------------------------------------------------------------------------
 // Main
 
 int main(int argc, char **argv)
 {
-  ObTools::Log::StreamChannel chan_out(cout);
-  ObTools::Log::StreamChannel chan_err(cerr);
+  StreamChannel chan_out(cout);
+  StreamChannel chan_err(cerr);
 
-  ObTools::Log::PatternFilter hfilter("H*", chan_out);
-  ObTools::Log::LevelFilter   level_out(ObTools::Log::LEVEL_SUMMARY, hfilter);
+  PatternFilter hfilter("H*", chan_out);
+  LevelFilter   level_out(LEVEL_SUMMARY, hfilter);
 
-  ObTools::Log::TimestampFilter tsfilter("%H:%M:%S %a %d %b %Y: ", chan_err);
-  ObTools::Log::LevelFilter     level_err(ObTools::Log::LEVEL_ERROR, tsfilter);
+  TimestampFilter tsfilter("%H:%M:%S %a %d %b %Y: ", chan_err);
+  LevelFilter     level_err(LEVEL_ERROR, tsfilter);
 
-  ObTools::Log::logger.connect(hfilter);
-  ObTools::Log::logger.connect(level_err);
+  logger.connect(hfilter);
+  logger.connect(level_err);
 
-  ObTools::Log::Summary << "Hello, world\nThis is a test\n\n";
-  ObTools::Log::Summary << "You shouldn't see this\n";
+  Summary << "Hello, world\nThis is a test\n\n";
+  Summary << "You shouldn't see this\n";
 
-  if (ObTools::Log::dump_ok)
-    ObTools::Log::Dump << "This is more than you ever wanted to know\n";
+  if (dump_ok)
+    Dump << "This is more than you ever wanted to know\n";
 
-  ObTools::Log::Error << "Hey, both cout and cerr should see this\n";
-  ObTools::Log::Error << "But only cerr will see this\nand this\n";
+  Error << "Hey, both cout and cerr should see this\n";
+  Error << "But only cerr will see this\nand this\n";
 
   return 0;  
 }
