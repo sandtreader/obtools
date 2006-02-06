e0b7539b100ee13b1a850a061ed185d859aed841
prc <prc>
2006-02-06 09:21:20 +0000

    Moved PKCS5 into new crypto library

--- libs/misc/ot-misc.h
+++ libs/misc/ot-misc.h
@@ -214,24 +214,6 @@ public:
 };
 
 //==========================================================================
-//PKCS5 padding support 
-class PKCS5
-{
- public:
-  //------------------------------------------------------------------------
-  // Pad a block of data to given length multiple
-  // Returns copied and padded malloc'ed data block, and modifies length
-  // to length of padded block
-  static unsigned char *pad(const unsigned char *data, int& length, 
-			    int multiple);
-
-  //------------------------------------------------------------------------
-  // Unpad a block of data 
-  // Returns original length of block - data is not copied or modified
-  static int PKCS5::original_length(const unsigned char *data, int length);
-};
-
-//==========================================================================
 }} //namespaces
 #endif // !__OBTOOLS_MISC_H
 
