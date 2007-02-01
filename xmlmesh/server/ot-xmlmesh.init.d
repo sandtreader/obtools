c6eb53e0f777f211ea0bb193d74e20aec7044035
prc <prc>
2007-02-01 17:30:15 +0000

    Change from maint-2-1 branch Add --oknodo to init.d stop

--- xmlmesh/server/ot-xmlmesh.init.d
+++ xmlmesh/server/ot-xmlmesh.init.d
@@ -9,11 +9,11 @@ start)	echo -n "Starting ObTools XMLMesh server: ot-xmlmesh"
         echo "." 
 	;;
 stop)	echo -n "Stopping ObTools XMLMesh server: ot-xmlmesh"
-        start-stop-daemon --stop --quiet --pidfile /var/run/ot-xmlmesh.pid --name ot-xmlmesh 
+        start-stop-daemon --stop --retry 5 --oknodo --quiet --pidfile /var/run/ot-xmlmesh.pid --name ot-xmlmesh 
         echo "."
         ;;
 restart|force-reload) echo -n "Restarting ObTools XMLMesh server"
-        start-stop-daemon --stop --retry 5 --quiet --pidfile /var/run/ot-xmlmesh.pid --name ot-xmlmesh
+        start-stop-daemon --stop --retry 5 --oknodo --quiet --pidfile /var/run/ot-xmlmesh.pid --name ot-xmlmesh
 	sleep 2
         start-stop-daemon --start --quiet --pidfile /var/run/ot-xmlmesh.pid --name ot-xmlmesh --startas /usr/sbin/ot-xmlmesh
         echo "."
