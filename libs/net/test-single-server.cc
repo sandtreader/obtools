9a90adc1f935ed895d1a23b782d9e26563d59748
prc <prc>
2007-12-04 16:33:11 +0000

    Test for single server

---
+++ libs/net/test-single-server.cc
@@ -0,0 +1,69 @@
+//==========================================================================
+// ObTools::Net: test-single-server.cc
+//
+// Test harness for TCP single-threaded server functions
+//
+// Copyright (c) 2007 xMill Consulting Limited.  All rights reserved
+// @@@ MASTER SOURCE - PROPRIETARY AND CONFIDENTIAL - NO LICENCE GRANTED
+//==========================================================================
+
+#include "ot-net.h"
+using namespace std;
+
+//--------------------------------------------------------------------------
+// Main
+
+int main(int argc, char **argv)
+{
+  int port = 11111;
+  if (argc > 1) port = atoi(argv[1]);
+
+#if defined(__WIN32__)
+  winsock_initialise();
+#endif
+
+  cout << "Starting server on port " << port << endl;
+  ObTools::Net::TCPSingleServer server(port);
+
+  for(;;)
+  {
+    ObTools::Net::TCPSocket *s = server.wait(5);
+
+    if (s)
+    {
+      ObTools::Net::EndPoint client = s->remote();
+      cout << "Connection from " << client << endl;
+
+      try
+      {
+	// Just output and reflect what we get
+	string buf;
+	while (*s >> buf)
+	{
+	  cout << buf;
+	  *s << "<< " << buf << "\n";
+	}
+
+	cout << "Connection from " << client << " ended\n";
+      }
+      catch (ObTools::Net::SocketError se)
+      {
+	cerr << se << endl;
+	return 2;
+      }
+
+      delete s;
+    }
+    else
+    {
+      cerr << "Can't listen on port " << port << endl;
+      return 4;
+    }
+  }
+
+  return 0;
+}
+
+
+
+
