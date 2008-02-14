d1a9f9e9f9a5cf4bb776e4be9a2de9a33c2d5df2
prc <prc>
2008-02-14 17:14:38 +0000

    Create Makefiles for generated code

---
+++ obcache/tests/build.mk
@@ -0,0 +1,17 @@
+#==========================================================================
+# Standard Makefile for ObTools ObCache generated code
+# This is copied into the build directories
+# 
+# Copyright (c) 2008 xMill Consulting Limited.  All rights reserved
+# @@@ MASTER SOURCE - PROPRIETARY AND CONFIDENTIAL - NO LICENCE GRANTED
+#==========================================================================
+
+# Top-level target
+all:
+	@echo I am in the sub-directory
+
+# Include auto-generated Makefiles
+include core.mk
+include sql.mk
+
+
---
+++ obcache/tests/model.mk
@@ -0,0 +1,66 @@
+#==========================================================================
+# Standard Makefile for ObTools ObCache generated code
+# Work without the ObTools build environment
+# Include from test directory, defining XMI to source XMI file
+# 
+# Copyright (c) 2008 xMill Consulting Limited.  All rights reserved
+# @@@ MASTER SOURCE - PROPRIETARY AND CONFIDENTIAL - NO LICENCE GRANTED
+#==========================================================================
+
+# Set DEV for development within the ObCache world, otherwise
+# it uses installed packages
+# e.g.
+#   $ make DEV=1
+
+# Build directory
+BUILD-DIR=build
+
+# Code generation tools
+ifdef DEV
+
+# Development versions using development config
+# Note this is relative to build directory
+GEN=../../../gen
+OT-GENERATE-CORE-CPP=$(GEN)/core/build-debug/ot-generate-core-cpp $(GEN)/core/ot-generate-core-cpp.cfg.xml
+OT-GENERATE-SQL-SCHEMA=$(GEN)/sql/build-debug/ot-generate-sql-schema $(GEN)/sql/ot-generate-sql-schema.cfg.xml
+OT-GENERATE-SQL-CPP=$(GEN)/sql/build-debug/ot-generate-sql-cpp $(GEN)/sql/ot-generate-sql-cpp.cfg.xml
+
+else
+
+# Installed versions, using global config
+OT-GENERATE-CORE-CPP=ot-generate-core-cpp
+OT-GENERATE-SQL-SCHEMA=ot-generate-sql-schema
+OT-GENERATE-SQL-CPP=ot-generate-sql-cpp
+
+endif
+
+# Main target
+all: build-dir generate sub-makefile sub-build
+
+# Create build directory
+build-dir:
+	-@mkdir build
+
+# Generate code inside build directory
+generate:
+	cd $(BUILD-DIR); $(OT-GENERATE-CORE-CPP) < ../$(XMI)    
+	cd $(BUILD-DIR); $(OT-GENERATE-SQL-SCHEMA) < ../$(XMI)
+	cd $(BUILD-DIR); $(OT-GENERATE-SQL-CPP) < ../$(XMI)        
+
+# Create Makefile for build directory
+sub-makefile:
+	cp ../build.mk $(BUILD-DIR)/Makefile
+
+# Call to constructed Makefile
+sub-build:
+	$(MAKE) -C $(BUILD-DIR)
+
+# Clean up
+clean:
+	-@rm -rf *~ $(BUILD_DIR)
+
+
+
+
+
+
