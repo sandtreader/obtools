65fba8b2a377ed7f9c107e8c9fe72dda3a958dd3
prc <prc>
2008-04-18 09:44:55 +0000

    Slightly safer CN fetch

--- libs/crypto/x509.cc
+++ libs/crypto/x509.cc
@@ -94,8 +94,8 @@ string Certificate::get_cn() const
 
   char buf[256];
   buf[0] = 0;
-  X509_NAME_get_text_by_NID(X509_get_subject_name(x509), NID_commonName,
-			    buf, 256);
+  X509_NAME *subject = X509_get_subject_name(x509);
+  if (subject) X509_NAME_get_text_by_NID(subject, NID_commonName, buf, 256);
   return string(buf);
 }
 
