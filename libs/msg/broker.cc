7738dcd0f44e514271b8a99f04ff0bf8537287c2
prc <prc>
2010-04-19 17:11:11 +0000

    Generic message brokering between (e.g.) SOAP and XMLMesh

---
+++ libs/msg/broker.cc
@@ -0,0 +1,88 @@
+//==========================================================================
+// ObTools::Message: broker.cc
+//
+// Implementation of message broker
+//
+// Copyright (c) 2010 Paul Clark.  All rights reserved
+// This code comes with NO WARRANTY and is subject to licence agreement
+//==========================================================================
+
+#include "ot-msg.h"
+#include "ot-log.h"
+
+namespace ObTools { namespace Message {
+
+//--------------------------------------------------------------------------
+// Add a transport
+void Broker::add_transport(Transport *trans)
+{
+  transports[trans->name] = trans;
+}
+
+//--------------------------------------------------------------------------
+// Configure from XML configuration (<messages> element)
+void Broker::configure(XML::Element& config)
+{
+  // Create all message handler modules in order
+  for(XML::Element::iterator p(config.children); p; ++p)
+  {
+    XML::Element& mhe = *p;
+    if (!mhe.name.empty() && !create_handler(mhe))
+    {
+      Log::Error log;
+      log << "Failed to create message handler from XML:\n" << mhe;
+    }
+  }
+}
+
+//--------------------------------------------------------------------------
+// Create a message handler from the given configuration element
+bool Broker::create_handler(XML::Element& xml)
+{
+  Handler *mh = handler_registry.create(xml.name, xml);
+  if (!mh) return false;
+  
+  // Store it
+  handlers.push_back(mh);
+
+  // Register in all transports with child elements in this one
+  for(XML::Element::iterator p(xml.children); p; ++p)
+  {
+    XML::Element& te = *p;
+    if (!te.name.empty())
+    {
+      map<string, Transport *>::iterator q = transports.find(te.name);
+      if (q != transports.end())
+      {
+	Transport *trans = q->second;
+	trans->register_handler(*mh, te);
+      }
+      else
+      {
+	Log::Error log;
+	log << "No such message transport: " << te.name << endl;
+      }
+    }
+  }
+
+  return true;
+}
+
+
+//--------------------------------------------------------------------------
+// Destructor
+Broker::~Broker()
+{
+  // Delete all transports first since they depend on the handlers
+  for(map<string, Transport *>::iterator p = transports.begin();
+      p != transports.end(); ++p)
+    delete p->second;
+
+  // Then the handlers
+  for(list<Handler *>::iterator p = handlers.begin(); p!=handlers.end(); ++p)
+    delete *p;
+}
+
+
+
+}} // namespaces
---
+++ libs/msg/ot-msg.h
@@ -0,0 +1,130 @@
+//==========================================================================
+// ObTools::Message: ot-msg.h
+//
+// Public definitions for ObTools::Message
+// Support for generic message/RPC broker
+// 
+// Copyright (c) 2010 Paul Clark.  All rights reserved
+// This code comes with NO WARRANTY and is subject to licence agreement
+//==========================================================================
+
+#ifndef __OBTOOLS_MSG_H
+#define __OBTOOLS_MSG_H
+
+#include "ot-xml.h"
+#include "ot-init.h"
+
+namespace ObTools { namespace Message { 
+
+//Make our lives easier without polluting anyone else
+using namespace std;
+
+//==========================================================================
+// Unified message handler interface (abstract)
+class Handler
+{
+public:
+  string name;                  // Handler name
+  string document_name;         // Message document name prefix
+  bool complex_result;          // Is there a complex result, or just 'ok'?
+  string ns_prefix;             // Namespace prefix
+  string ns_url;                // Namespace URL
+
+  //--------------------------------------------------------------------------
+  // Default constructor - the above filled in in subclass constructors
+  Handler(XML::Element& cfg, const string& _doc_name,
+	  const string& _ns_prefix="", const string& _ns_url="",
+	  bool _complex_result = false):
+    name(cfg.name),
+    document_name(_doc_name),
+    complex_result(_complex_result),
+    ns_prefix(_ns_prefix),
+    ns_url(_ns_url)
+  {}
+
+  //--------------------------------------------------------------------------
+  // Call to handle message and respond
+  // Returns error string or "" if OK, in which case response is filled in
+  // Response document will be pre-created with response_document_name
+  virtual string handle_message(const XML::Element& request, 
+				XML::Element& response) = 0;
+
+  //--------------------------------------------------------------------------
+  // Virtual destructor (probably not needed)
+  virtual ~Handler() {}
+};
+
+//==========================================================================
+// Message transport interface - register message handlers against underlying
+// transport (abstract)
+class Transport
+{
+ public:
+  string name;  // Short name for config
+
+  //--------------------------------------------------------------------------
+  // Constructor
+  Transport(const string& _name): name(_name) {}
+
+  //--------------------------------------------------------------------------
+  // Register a handler with the given config element
+  virtual void register_handler(Handler& handler, XML::Element& config) = 0;
+
+  //--------------------------------------------------------------------------
+  // Virtual destructor
+  virtual ~Transport() {}
+};
+
+//==========================================================================
+// Unified message broker - accepts registration of multiple message handlers
+// into multiple message transports
+class Broker
+{
+  map<string, Transport *> transports;  // Transports mapped by name
+  list<Handler *> handlers;             // All registered handlers
+
+  // Internals
+  bool create_handler(XML::Element& xml);
+
+public:
+  // Registry of message handlers - provides automatic initialisation
+  Init::Registry<Handler> handler_registry;
+
+  //--------------------------------------------------------------------------
+  // Constructor
+  Broker() {}
+
+  //--------------------------------------------------------------------------
+  // Add a transport
+  void add_transport(Transport *trans);
+
+  //--------------------------------------------------------------------------
+  // Configure from XML configuration (<message> element)
+  void configure(XML::Element& config);
+
+  //--------------------------------------------------------------------------
+  // Destructor
+  ~Broker();
+};
+
+
+//==========================================================================
+}} //namespaces
+#endif // !__OBTOOLS_MSG_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
