9ebb5dc0d961af19defa660616cbe3180a9be128
Paul Clark <paul@packetship.com>
2012-09-03 17:52:18 +0100

    Made test finish in finite time, set RC

--- libs/ring/test-ring.cc
+++ libs/ring/test-ring.cc
@@ -1,4 +1,4 @@
-//==========================================================================
+#//==========================================================================
 // ObTools::Ring: test-ring.cc
 //
 // Test harness for ring buffer library
@@ -12,6 +12,9 @@
 #include "iostream"
 #include <stdlib.h>
 
+#define DEFAULT_BUFFER_LENGTH 100
+#define DEFAULT_ITERATIONS 10000000
+
 using namespace std;
 using namespace ObTools;
 
@@ -23,11 +26,15 @@ typedef Ring::Buffer<int> TestBuffer;
 class ReadThread: public MT::Thread
 {
   TestBuffer& buffer;
+  bool running;
   virtual void run();
 
 public:
-  ReadThread(TestBuffer& _buffer): buffer(_buffer)
+  int errors;
+  ReadThread(TestBuffer& _buffer): buffer(_buffer), running(true), errors(0)
   { start(); }
+
+  void stop() { running = false; }
 };
 
 void ReadThread::run()
@@ -35,13 +42,16 @@ void ReadThread::run()
   int next_n = 0;
 
   // Read as fast as possible
-  for(;;)
+  while (running)
   {
     int n;
     if (buffer.get(n))
     {
       if (n != next_n)
+      {
 	cerr << "Out of phase - expected " << next_n << " got " << n << endl;
+        errors++;
+      }
 
       //      cout << n << endl;
       next_n = n+1;
@@ -54,24 +64,31 @@ void ReadThread::run()
 // Main
 int main(int argc, char **argv)
 {
-  int length = 100;
-  if (argc > 1) length = atoi(argv[1]);
+  int iterations = DEFAULT_ITERATIONS;
+  if (argc > 1) iterations = atoi(argv[1]);
+
+  int length = DEFAULT_BUFFER_LENGTH;
+  if (argc > 2) length = atoi(argv[2]);
+
   TestBuffer buffer(length);
   cerr << "Buffer length " << length << endl;
+  cerr << iterations << " iterations\n";
 
   // Start reader thread
   ReadThread read_thread(buffer);
 
   // Write as fast as possible
   int n=0;
-  for(;;)
+  while (n < iterations)
   {
-    if (buffer.put(n)) 
+    if (buffer.put(n))
       n++;
     //else cout << "+";
   }
 
-  return 0;  
+  read_thread.stop();
+  cerr << read_thread.errors << " errors\n";
+  return read_thread.errors?2:0;
 }
 
 
