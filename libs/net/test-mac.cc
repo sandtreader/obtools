1642af87790863ba74a31dd3b9ca5320027a66fd
Paul Clark <paul@packetship.com>
2012-11-12 16:05:23 +0000

    Fix using a result from ip neigh show
    Otherwise running on testhost itself (=gig) fails

--- libs/net/test-mac.cc
+++ libs/net/test-mac.cc
@@ -17,9 +17,20 @@ using namespace ObTools;
 
 TEST(MACTests, TestMACValid)
 {
+  // Look in ip neigh show for someone we know
+  FILE *f = popen("/bin/ip neigh show | head -n 1 | cut -d\" \" -f 1", "r");
+  ASSERT_TRUE(f != NULL) << "Can't run `ip neigh show`";
+
+  char line[81];
+  fgets(line, sizeof(line)-1, f);
+  ASSERT_GT(strlen(line), 0);
+  line[strlen(line)-1] = 0; // chomp NL
+  fclose(f);
+
   Net::TCPSocket socket;
-  string mac = socket.get_mac(Net::IPAddress("testhost"));
+  string mac = socket.get_mac(Net::IPAddress(line));
   ASSERT_NE(mac, "");
+  EXPECT_EQ(mac.size(), 17);
 
   // Check it has 5 colons and nothing outside hex range
   int colons = 0;
