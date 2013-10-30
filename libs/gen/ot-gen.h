5a70fb1821f62fdaf8e89e57737bf622fb38840e
Alex Woods <alex@packetship.com>
2013-10-30 15:04:35 +0000

    Add UniquePointer.
    - it's not really a unique pointer, but it's a container-safe smart
      pointer that will do until C++11

--- libs/gen/ot-gen.h
+++ libs/gen/ot-gen.h
@@ -131,5 +131,120 @@ ostream& operator<<(ostream& os, const SharedPointer<T>& shared_pointer)
 }
 
 //==========================================================================
+// Unique Pointer
+// !! This isn't really a unique pointer, rather just a copy of shared pointer
+// !! Replace with std::unique_ptr when the move to C++11 or above happens
+template<class T>
+class UniquePointer
+{
+private:
+  unsigned int *ref_count;
+  T *pointer;
+
+  //------------------------------------------------------------------------
+  // Release our reference to pointer
+  // Deletes pointer and reference count if no instances remain
+  void release()
+  {
+    if (!--*ref_count)
+    {
+      if (pointer)
+        delete pointer;
+      delete ref_count;
+    }
+  }
+
+public:
+  //------------------------------------------------------------------------
+  // Constructor from pointer
+  UniquePointer(T *_pointer = 0):
+    ref_count(new unsigned int(1)), pointer(_pointer)
+  {}
+
+  //------------------------------------------------------------------------
+  // Copy constructor
+  UniquePointer(const UniquePointer<T>& unique_pointer):
+    ref_count(unique_pointer.ref_count), pointer(unique_pointer.pointer)
+  {
+    ++*ref_count;
+  }
+
+  //------------------------------------------------------------------------
+  // Reset function - release and point to something else
+  void reset(T *_pointer = 0)
+  {
+    release();
+    ref_count = new unsigned int(1);
+    pointer = _pointer;
+  }
+
+  //------------------------------------------------------------------------
+  // Equality operator
+  bool operator==(const UniquePointer<T>& unique_pointer) const
+  {
+    return pointer == unique_pointer.pointer;
+  }
+
+  //------------------------------------------------------------------------
+  // Inequality operator
+  bool operator!=(const UniquePointer<T>& unique_pointer) const
+  {
+    return !operator==(unique_pointer);
+  }
+
+  //------------------------------------------------------------------------
+  // Not operator (check if invalid)
+  bool operator!() const
+  {
+    return !pointer;
+  }
+
+  //------------------------------------------------------------------------
+  // Assignment operator
+  UniquePointer& operator=(const UniquePointer<T>& unique_pointer)
+  {
+    release();
+    ref_count = unique_pointer.ref_count;
+    pointer = unique_pointer.pointer;
+    ++*ref_count;
+    return *this;
+  }
+
+  //------------------------------------------------------------------------
+  // Get pointer to target
+  T* get() const
+  {
+    return pointer;
+  }
+
+  //------------------------------------------------------------------------
+  // Pointer operators
+  T& operator*() const
+  {
+    return *pointer;
+  }
+
+  T* operator->() const
+  {
+    return pointer;
+  }
+
+  //------------------------------------------------------------------------
+  // Destructor
+  ~UniquePointer()
+  {
+    release();
+  }
+};
+
+//--------------------------------------------------------------------------
+// Stream output handler
+template<class T>
+ostream& operator<<(ostream& os, const UniquePointer<T>& unique_pointer)
+{
+  return os << "Gen::UniquePointer *" << unique_pointer.get();
+}
+
+//==========================================================================
 }} //namespaces
 #endif // !__OBTOOLS_GEN_H
