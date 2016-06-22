55fd69728339b12247f093facdeddac4ac8ca4ab
Alex Woods <alex@packetship.com>
2016-06-22 16:25:19 +0100

    Debian stuff for tup.
    bug:comment:102

---
+++ build/create-deb.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -e
+
+NAME=$1
+VERSION=$2
+REVISION=$3
+ARCH=$4
+
+DEBDIR=debian
+
+CHANGELOG=$DEBDIR/changelog
+COMPAT=$DEBDIR/compat
+RULES=$DEBDIR/rules
+REMOVE=
+
+cp -r DEBIAN $DEBDIR
+
+if [ ! -e $CHANGELOG ]
+then
+  cat << EOF > $DEBDIR/changelog
+$NAME ($VERSION-$REVISION) stable; urgency=low
+
+  * See documentation.
+
+ -- Paul Clark <paul@packetship.com>  `date -R`
+EOF
+fi
+
+if [ ! -e $COMPAT ]
+then
+  cat << EOF > $COMPAT
+9
+EOF
+fi
+
+if [ ! -e $RULES ]
+then
+  cat << 'EOF' > $RULES
+#!/usr/bin/make -f
+
+%:
+	dh $@
+EOF
+  chmod a+x $DEBDIR/rules
+fi
+
+dpkg-buildpackage -uc -b -rfakeroot-ng -tc
+mv ../${NAME}_${VERSION}-${REVISION}_${ARCH}.deb ./
+rm ../${NAME}_${VERSION}-${REVISION}_*.changes
+rm -rf $DEBDIR
