22e73da037fb805cf8d3aa9d3862404985a31d2e
Jon Barber <jon@packetship.com>
2016-07-14 13:10:40 +0100

    Build automation helpers

--- build/init.sh
+++ build/init.sh
@@ -5,3 +5,44 @@ ln -s $BUILD_DIR/Tuprules.tup
 ln -s $BUILD_DIR/Tupdepends.lua
 ln -s $BUILD_DIR/Tupsources.lua
 ln -s $BUILD_DIR/create-deb.sh
+
+while getopts ":t:" opt; do
+  case $opt in
+    t)
+      buildtype=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+   :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ "$buildtype" ]; then
+  HOSTTYPE=`uname -m`
+  TARGARCH=i386
+
+  if [ "$HOSTTYPE" =  "x86_64" ]; then
+    TARGARCH=amd64
+  fi
+
+  for config in $buildtype.$TARGARCH $buildtype
+  do
+    if [ -f $BUILD_DIR/tup.config.$config ]; then
+      mkdir build-$buildtype
+      cp $BUILD_DIR/tup.config.$config build-$buildtype/tup.config
+      FOUNDCONFIG=1
+      break
+    fi
+  done
+  if [ $FOUNDCONFIG ]; then
+    tup init
+  else
+    echo "`basename $0`: can't find config for $buildtype"
+  fi
+fi
+
--- xmlmesh/bindings/php/test.php
+++ xmlmesh/bindings/php/test.php
@@ -1,7 +1,14 @@
 <?php
   include ("ot-xmlmesh.php");
+    global $xmlmesh_host;
+    $xmlmesh_host = "testhost";
 
   if (xmlmesh_simple_request("foo", "<foo/>"))
+    print "OK\n";
+  else
+    print "Error: $xmlmesh_last_error\n";
+
+  if (xmlmesh_request("foo", "<foo/>"))
     print "OK\n";
   else
     print "Error: $xmlmesh_last_error\n";
