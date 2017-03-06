ee10cba93974458686003f90437b0ad5bef68078
Paul Clark <paul@packetship.com>
2017-03-06 15:57:09 +0000

    Rework to allow dynamic subscription/unsub within a single poll
    Previous relationship is now inverted - the client should now start
    a poll/xxx request and then add subscribe/xxx requests within it.
    It can also unsubscribe/xxx at will.
    
    A further API change - now pollAndSubscribe() reflecting the new
    order.
    
    bug:comment:808

--- xmlmesh/bindings/js/ot-xmlmesh.js
+++ xmlmesh/bindings/js/ot-xmlmesh.js
@@ -157,106 +157,10 @@
         });
     },
 
-    // Subscribe for a subject pattern with an optional ID
+    // Set up a message poller
+    // Must be in place before subscribes can be issued (with the same ref)
     // {
-    //   pattern:  Subject pattern with glob
-    //   ref:      Optional ref (or random one invented)
-    // }
-    // Also returns result.ref which is unique reference used
-    subscribe: function(params)
-    {
-      this.log("Subscribing for "+params.pattern);
-      var message = "<x:join subject='"+params.pattern+"'/>";
-      var soap = makeSOAP("xmlmesh.subscription.join", true, message);
-      this.log(soap);
-
-      var ref = params.ref || uniqueID();
-      this.last_ref = ref;
-      var url = this.url_prefix+"/subscribe/"+ref;
-      var self = this;
-      $.ajax(
-        {
-          type: "post",
-          url: url,
-          data: soap,
-          dataType: "text",
-
-          error:function(jqXHR, textStatus, errorThrown)
-          {
-            self.log("Subscribe error: " + textStatus + " " + errorThrown);
-            if (params.completion) params.completion(
-              {
-                success: false,
-                error: textStatus+" "+errorThrown
-              });
-          },
-
-          success: function(response)
-          {
-            self.log("Response: "+response);
-
-            var result =
-            {
-              success: true,
-              ref: ref
-            };
-            if (params.rsvp) result.response = parseSOAP(response).body;
-            if (params.completion) params.completion(result);
-          }
-        });
-    },
-
-    // Unsubscribe for a subject pattern
-    // {
-    //   pattern:  Subject pattern with glob
-    //   ref:      Mandatory ref used in subscribe()
-    // }
-    unsubscribe: function(params)
-    {
-      this.log("Unsubscribing for "+params.pattern);
-      var message = "<x:leave subject='"+params.pattern+"'/>";
-      var soap = makeSOAP("xmlmesh.subscription.leave", true, message);
-      this.log(soap);
-
-      var ref = params.ref;
-      var url = this.url_prefix+"/unsubscribe/"+ref;
-      var self = this;
-      $.ajax(
-        {
-          type: "post",
-          url: url,
-          data: soap,
-          dataType: "text",
-
-          error:function(jqXHR, textStatus, errorThrown)
-          {
-            self.log("Unsubscribe error: " + textStatus + " " + errorThrown);
-            if (params.completion) params.completion(
-              {
-                success: false,
-                error: textStatus+" "+errorThrown
-              });
-          },
-
-          success: function(response)
-          {
-            self.log("Response: "+response);
-
-            var result =
-            {
-              success: true,
-              ref: ref
-            };
-            if (params.rsvp) result.response = parseSOAP(response).body;
-            if (params.completion) params.completion(result);
-          }
-        });
-    },
-
-    // Poll for a message from an earlier subscribe()
-    // {
-    //   ref:   Optional ref from subscribe, otherwise uses last ref
-    //          subscribed with
+    //   ref:   Optional ref (or random one invented)
     //   retry: true to automatically retry polls
     //   id:    poll ID returned to callback
     // }
@@ -267,7 +171,8 @@
     {
       this.log("Polling"+(params.id?" ID "+params.id:"")
                         +(params.retry?" with retry":""));
-      var ref = params.ref || this.last_ref || "unknown";
+      var ref = params.ref || uniqueID();
+      this.last_ref = ref;
       var url = this.url_prefix+"/poll/"+ref;
       var self = this;
       $.ajax(
@@ -321,12 +226,104 @@
         });
     },
 
+    // Subscribe for a subject pattern with an optional ID
+    // {
+    //   pattern:  Subject pattern with glob
+    //   ref:      Optional ref, defaults to last poll() ref
+    // }
+    subscribe: function(params)
+    {
+      this.log("Subscribing for "+params.pattern);
+      var message = "<x:join subject='"+params.pattern+"'/>";
+      var soap = makeSOAP("xmlmesh.subscription.join", true, message);
+      this.log(soap);
+
+      var ref = params.ref || this.last_ref;
+      var url = this.url_prefix+"/subscribe/"+ref;
+      var self = this;
+      $.ajax(
+        {
+          type: "post",
+          url: url,
+          data: soap,
+          dataType: "text",
+
+          error:function(jqXHR, textStatus, errorThrown)
+          {
+            self.log("Subscribe error: " + textStatus + " " + errorThrown);
+            if (params.completion) params.completion(
+              {
+                success: false,
+                error: textStatus+" "+errorThrown
+              });
+          },
+
+          success: function(response)
+          {
+            self.log("Response: "+response);
+
+            var result =
+            {
+              success: true
+            };
+            if (params.rsvp) result.response = parseSOAP(response).body;
+            if (params.completion) params.completion(result);
+          }
+        });
+    },
+
+    // Unsubscribe for a subject pattern
+    // {
+    //   pattern:  Subject pattern with glob
+    //   ref:      Optional ref, defaults to last poll() ref
+    // }
+    unsubscribe: function(params)
+    {
+      this.log("Unsubscribing for "+params.pattern);
+      var message = "<x:leave subject='"+params.pattern+"'/>";
+      var soap = makeSOAP("xmlmesh.subscription.leave", true, message);
+      this.log(soap);
+
+      var ref = params.ref || this.last_ref;
+      var url = this.url_prefix+"/unsubscribe/"+ref;
+      var self = this;
+      $.ajax(
+        {
+          type: "post",
+          url: url,
+          data: soap,
+          dataType: "text",
+
+          error:function(jqXHR, textStatus, errorThrown)
+          {
+            self.log("Unsubscribe error: " + textStatus + " " + errorThrown);
+            if (params.completion) params.completion(
+              {
+                success: false,
+                error: textStatus+" "+errorThrown
+              });
+          },
+
+          success: function(response)
+          {
+            self.log("Response: "+response);
+
+            var result =
+            {
+              success: true
+            };
+            if (params.rsvp) result.response = parseSOAP(response).body;
+            if (params.completion) params.completion(result);
+          }
+        });
+    },
+
     // Multiplexed subscribe - uses a single XMLMesh connection and
     // demultiplexes the results before passing back.  Handles its own
     // polling internally.
     // callback is called with XML message body ($) and id, or null and id
     // for a poll timeout - in either case return false to unsubscribe again
-    subscribeAndPoll: function(pattern, callback, id)
+    pollAndSubscribe: function(pattern, callback, id)
     {
       var self=this;
 
@@ -334,18 +331,67 @@
       // will be used for all subscribes as well
       if (!this.poller_ref) this.poller_ref = uniqueID();
 
-      // If creation of poller wanted, agree to do it later to prevent race
-      // Note in current server, at least one subscribe request must be sent
-      // on a given ref before the poller can be activated - if that ever
-      // changed, this could be simpler.
-      var start_poller = false;
+      // Function to unsubscribe, given index in subscriptions array
+      function unsub(sub_index)
+      {
+        var sub = self.subscribers[sub_index];
+        self.unsubscribe({ pattern: sub.pattern,
+                           ref: self.poller_ref });
+
+        // Remove from our list
+        self.subscribers.splice(sub_index, 1);
+      }
+
+      // Start poller if not already done
       if (!this.poller_started)
       {
+        // Start permanent poller
+        self.poll({
+          ref: self.poller_ref,
+          retry: true,
+          completion: function(poll_result)
+          {
+            if (poll_result.success)
+            {
+              // Call any matching subscribers with this message
+              for(var i=0; i<self.subscribers.length; i++)
+              {
+                var sub = self.subscribers[i];
+
+                if (glob(sub.pattern, poll_result.subject)
+                    && !sub.callback(poll_result.response, sub.id))
+                  unsub(i);
+              }
+              return true;  // Always keep running
+            }
+            else if (poll_result.error)
+            {
+              self.log("Poll failed: "+poll_result.error);
+              self.poller_started = false;
+              return false;  // Forget this one, try again
+            }
+            else // Poll timed out
+            {
+              // Call all subscribers with null so they get a chance
+              // to unsubscribe
+              for(var i=0; i<self.subscribers.length; i++)
+              {
+                var sub = self.subscribers[i];
+                if (!sub.callback(null, sub.id))
+                  unsub(i);
+              }
+
+              // But continue the poller anyway
+              return true;
+            }
+          }
+        });
+
         start_poller = true;
         this.poller_started = true;
       }
 
-      // Do subscription
+      // Subscribe
       this.subscribe({
         pattern: pattern,
         ref: this.poller_ref,
@@ -357,78 +403,18 @@
             self.subscribers.push({ pattern: pattern,
                                     callback: callback,
                                     id: id });
-
-            if (start_poller)
-            {
-              function unsub(sub_index)
-              {
-                var sub = self.subscribers[sub_index];
-                self.unsubscribe({ pattern: sub.pattern,
-                                   ref: self.poller_ref });
-
-                // Remove from our list
-                self.subscribers.splice(sub_index, 1);
-              }
-
-              // Start permanent poller
-              self.poll({
-                ref: self.poller_ref,
-                retry: true,
-                completion: function(poll_result)
-                {
-                  if (poll_result.success)
-                  {
-                    // Call any matching subscribers with this message
-                    for(var i=0; i<self.subscribers.length; i++)
-                    {
-                      var sub = self.subscribers[i];
-
-                      if (glob(sub.pattern, poll_result.subject)
-                          && !sub.callback(poll_result.response, sub.id))
-                        unsub(i);
-                    }
-                    return true;  // Always keep running
-                  }
-                  else if (poll_result.error)
-                  {
-                    self.log("Poll failed: "+poll_result.error);
-                    self.poller_started = false;
-                    return false;  // Forget this one, try again
-                  }
-                  else // Poll timed out
-                  {
-                    // Call all subscribers with null so they get a chance
-                    // to unsubscribe
-                    for(var i=0; i<self.subscribers.length; i++)
-                    {
-                      var sub = self.subscribers[i];
-                      if (!sub.callback(null, sub.id))
-                        unsub(i);
-                    }
-
-                    // But continue the poller anyway
-                    return true;
-                  }
-
-                }
-              });
-            }
           }
           else
           {
             self.log("Subscription failed: "+sub_result.error)
 
-            // If we said we'd start the poller, own up
-            if (start_poller) self.poller_started = false;
-
             // Try again after a reasonable timeout
             setTimeout(function()
-                       { self.subscribeAndPoll(pattern, callback, id); },
+                       { self.pollAndSubscribe(pattern, callback, id); },
                        5000); // ? make optional parameter?
           }
         }
       });
-
     }
   });
 }(jQuery));
