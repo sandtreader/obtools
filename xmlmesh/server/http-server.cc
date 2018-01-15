e0dec288760b295eedad011e3bd454484ceeba77
Paul Clark <paul@packetship.com>
2018-01-15 17:56:46 +0000

    Add prefix to soak test
    To trigger the leak in the old version, do
    
    $ ./test-http-poll-soak DEAD
    
    - let it run to establish the polls (about 15 sec) up to fooDEAD500
    - then stop it and run
    
    $ ./test-http-poll-soak
    
    This will create poll IDs up to foo500, and then send messages every
    5 sec.  The zombie polls fooDEADxxx will soak up the messages but
    they are never removed, so it leaks - check top 'M' to see RES increasing
    at a few hundred K per 5 second period.
    
    You can also check the fix by extending the <subscription timeout> to
    "5 mins" and running it again as above.  Memory should increase while
    the DEAD ones are still there, but after 5 mins they should time out
    (there will be lines in the log) and the memory should drop back to
    base level again.
    
    bug:comment:808

--- xmlmesh/server/http-server.cc
+++ xmlmesh/server/http-server.cc
@@ -429,7 +429,6 @@ void HTTPServerService::tick()
 {
   client_request_map.tidy();
   active_poller_map.tidy();
-  client_poll_map.tidy();
 }
 
 //--------------------------------------------------------------------------
