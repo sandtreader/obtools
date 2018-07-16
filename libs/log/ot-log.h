8edde7e99c7d54cd0b4c3f3997c3e4fe6f1637ea
Alex Woods <alex@packetship.com>
2018-07-16 14:17:15 +0100

    Add move constuctors for Logs.
    bug:comment:1210

--- libs/log/ot-log.h
+++ libs/log/ot-log.h
@@ -323,10 +323,22 @@ class Stream: public ostream
 public:
   // Constructor - like ofstream
   Stream(Channel &channel, Level level):
-    ostream(new StreamBuf(channel, level)) {}
+    ostream(new StreamBuf(channel, level))
+  {
+  }
+
+  //------------------------------------------------------------------------
+  // Move constructor
+  Stream(Stream&& s):
+    ostream(std::move(s)) // Had to use std namespace
+                          // - wtf is the other move()?
+  {
+    set_rdbuf(rdbuf());
+    s.set_rdbuf(nullptr);
+  }
 
   // Destructor
-  ~Stream() { delete rdbuf(); }
+  ~Stream() { if (rdbuf()) delete rdbuf(); }
 
   // Close - pass on to StreamBuf to do
   void close() { static_cast<StreamBuf *>(rdbuf())->close(); }
@@ -374,18 +386,21 @@ class Error: public Stream
 {
 public:
   Error(): Stream(logger, Level::error) {}
+  Error(Error&& e): Stream(std::move(e)) {}
 };
 
 class Summary: public Stream
 {
 public:
   Summary(): Stream(logger, Level::summary) {}
+  Summary(Summary&& s): Stream(std::move(s)) {}
 };
 
 class Detail: public Stream
 {
 public:
   Detail(): Stream(logger, Level::detail) {}
+  Detail(Detail&& d): Stream(std::move(d)) {}
 };
 
 #if OBTOOLS_LOG_DEBUG
@@ -393,6 +408,7 @@ class Debug: public Stream
 {
 public:
   Debug(): Stream(logger, Level::debug) {}
+  Debug(Debug&& d): Stream(std::move(d)) {}
 };
 #endif
 
@@ -401,6 +417,7 @@ class Dump: public Stream
 {
 public:
   Dump(): Stream(logger, Level::dump) {}
+  Dump(Dump&& d): Stream(std::move(d)) {}
 };
 #endif
 
