5b06265c548a9b6c00602ad178006e22552f2653
Jon Barber <jon@packetship.com>
2018-09-18 16:04:26 +0100

    Build rpm if CentOS detected
    bug:comment:1271

--- build/create-deb.sh
+++ build/create-deb.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+DISTRO=$(lsb_release -s -i)
+
 VERSION=$1
 REVISION=$2
 NAMES=""
@@ -60,15 +62,44 @@ if [ -f /usr/local/bin/pseudo ]; then
   FAKEROOT=-rpseudo
 fi
 
+
+if [ "$DISTRO" = "CentOS" ]; then
+  FAKEROOT=-rfakeroot
+  sed  -i 's/^Build-Depends: debhelper (>= 9)$/#Build-Depends:/g' debian/control
+  cat <<EOF >> $RULES
+override_dh_shlibdeps:
+EOF
+
+  # find build (scripts) directory
+  RPM_DIR_DEPTH=0
+  RPM_MAX_DEPTH=5
+  RPM_MID=..
+  while [ ! -d ${RPM_MID}/build -a $RPM_DIR_DEPTH -lt $RPM_MAX_DEPTH ]
+  do
+    RPM_MID=${RPM_MID}/..
+    RPM_DIR_DEPTH=$(( ${RPM_DIR_DEPTH} + 1 ))
+  done
+
+  BUILD_DIR=${RPM_MID}/build
+fi
+
 if [ `id -u` -eq 0 ]; then
   FAKEROOT=
 fi
 
 dpkg-buildpackage -uc -b $FAKEROOT -tc
+
 for NAME in $NAMES
 do
-  mv ../${NAME}_${VERSION}-${REVISION}_*.deb ./
+  PACKAGE=${NAME}_${VERSION}-${REVISION}_*.deb
+  mv ../${PACKAGE} ./
   rm ../${NAME}_${VERSION}-${REVISION}_*.changes
   rm -f ../${NAME}_${VERSION}-${REVISION}_*.buildinfo
+
+  if [ "$DISTRO" = "CentOS" ]; then
+    . $BUILD_DIR/create-rpm.sh
+    rm $PACKAGE
+  fi
 done
+
 rm -rf $DEBDIR
