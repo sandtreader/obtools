a672fe5a361a1d87acc11601f2876e917c95e3db
Paul Clark <paul@packetship.com>
2019-01-03 12:35:19 +0000

    Simple Duration::ntp() test

---
+++ libs/time/test-duration.cc
@@ -0,0 +1,29 @@
+//==========================================================================
+// ObTools::Time: test-duration.cc
+//
+// Test Duration functions
+//
+// Copyright (c) 2019 Paul Clark.  All rights reserved
+// This code comes with NO WARRANTY and is subject to licence agreement
+//==========================================================================
+
+#include <gtest/gtest.h>
+#include "ot-time.h"
+
+using namespace std;
+using namespace ObTools;
+
+TEST(DurationTest, TestConvertToNTP)
+{
+  Time::Duration d(3600);
+  EXPECT_EQ(15461882265600ULL, d.ntp());
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+
+
