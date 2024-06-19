c003f9bf287de16615d8260616a3809a4acaecb6
Alex Woods <alexander.j.woods@gmail.com>
2024-06-19 15:48:15 +0100

    Removing hashing from EVP signature verification

--- libs/crypto/evp.cc
+++ libs/crypto/evp.cc
@@ -17,17 +17,29 @@ namespace ObTools { namespace Crypto { namespace EVP {
 bool verify(const EVPKey& key, const vector<byte>& message,
             const vector<byte>& signature)
 {
-  auto mdctx = unique_ptr<EVP_MD_CTX, void (*)(EVP_MD_CTX *)>{
-      EVP_MD_CTX_new(), EVP_MD_CTX_free};
-  auto hash_func = static_cast<const EVP_MD *>(nullptr);
-  if (key.type == EVPKey::Type::SECP256K1)
-    hash_func = EVP_sha256();
-  if (EVP_DigestVerifyInit(
-        mdctx.get(), nullptr, hash_func, nullptr, key.evp_key.get()) != 1)
-    throw runtime_error("Failed to initiliase EVP digest verifier");
-  return EVP_DigestVerify(mdctx.get(),
+  if (key.type == EVPKey::Type::X25519 || key.type == EVPKey::Type::Ed25519 ||
+      key.type == EVPKey::Type::X448 || key.type == EVPKey::Type::Ed448)
+  {
+    // EVP_PKEY_verify_init does not seem to support these types
+    auto mdctx = unique_ptr<EVP_MD_CTX, void (*)(EVP_MD_CTX *)>{
+        EVP_MD_CTX_new(), EVP_MD_CTX_free};
+    if (EVP_DigestVerifyInit(
+          mdctx.get(), nullptr, nullptr, nullptr, key.evp_key.get()) != 1)
+      throw runtime_error("Failed to initiliase EVP digest verifier");
+    return EVP_DigestVerify(mdctx.get(),
+        reinterpret_cast<const unsigned char *>(signature.data()),
+        signature.size(),
+        reinterpret_cast<const unsigned char *>(message.data()),
+        message.size()) == 1;
+  }
+  auto ctx = unique_ptr<EVP_PKEY_CTX, void (*)(EVP_PKEY_CTX *)>{
+      EVP_PKEY_CTX_new(key.evp_key.get(), nullptr), EVP_PKEY_CTX_free};
+  if (EVP_PKEY_verify_init(ctx.get()) != 1)
+    throw runtime_error("Failed to initiliase EVP verifier");
+  return EVP_PKEY_verify(ctx.get(),
       reinterpret_cast<const unsigned char *>(signature.data()),
-      signature.size(), reinterpret_cast<const unsigned char *>(message.data()),
+      signature.size(),
+      reinterpret_cast<const unsigned char *>(message.data()),
       message.size()) == 1;
 }
 
